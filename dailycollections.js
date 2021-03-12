0becc377149da01295dc8be2f355049d6b6a37c5
Softonic254 <rotichenoch27@gmail.com>
2021-03-12 08:21:58 +0300

    It will get user data

--- dailycollections.js
+++ dailycollections.js
@@ -57,7 +57,7 @@ function calculate() {
     if (isNaN(sA) || isNaN(sB) || isNaN(sC) | isNaN(sD)) {
         alert("Incomplete/No records found");
     } else {
-        document.getElementById("wkincome").innerHTML="Weekly and yearly income"
+        document.getElementById("wkincome").innerHTML = "Weekly and yearly income"
         document.getElementById("weekly").innerHTML = "Your weekly production is " + weekly + " litres.";
         document.getElementById("yearly").innerHTML = "Your yearly production will be " + yearly + " litres.";
     }
@@ -65,7 +65,7 @@ function calculate() {
 }
 
 function monthly() {
-    var month = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "Novermber", "December"];
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novermber", "December"];
 
     shedA = document.getElementById('shed_A').value;
     shedB = document.getElementById('shed_B').value;
@@ -81,29 +81,29 @@ function monthly() {
     var dailytotals = arraySheds[0] + arraySheds[1] + arraySheds[2] + arraySheds[3];
 
     var weekly = dailytotals * 7;
-    var one=7*31*dailytotals;
-    var zero=30*7*dailytotals;
+    var one = 7 * 31 * dailytotals;
+    var zero = 30 * 7 * dailytotals;
 
     if (isNaN(sA) || isNaN(sB) || isNaN(sC) | isNaN(sD)) {
         alert("Incomplete/No records found");
     } else {
-        document.getElementById("Monthlyincome").innerHTML="Income per month"
-        document.getElementById("jan").innerHTML="Your income for the month of "+month[0]+" will be Kshs."+one+".";
-        document.getElementById("feb").innerHTML="Your income for the month of "+month[1]+" will be Kshs."+zero+".";
-        document.getElementById("mar").innerHTML="Your income for the month of "+month[2]+" will be Kshs."+one+".";
-        document.getElementById("apr").innerHTML="Your income for the month of "+month[3]+" will be Kshs."+zero+".";
-        document.getElementById("may").innerHTML="Your income for the month of "+month[4]+" will be Kshs."+one+".";
-        document.getElementById("june").innerHTML="Your income for the month of "+month[5]+" will be Kshs."+zero+".";
-        document.getElementById("july").innerHTML="Your income for the month of "+month[6]+" will be Kshs."+one+".";
-        document.getElementById("aug").innerHTML="Your income for the month of "+month[7]+" will be Kshs."+zero+".";
-        document.getElementById("sep").innerHTML="Your income for the month of "+month[8]+" will be Kshs."+one+".";
-        document.getElementById("oct").innerHTML="Your income for the month of "+month[9]+" will be Kshs."+zero+".";
-        document.getElementById("nov").innerHTML="Your income for the month of "+month[10]+" will be Kshs."+one+".";
-        document.getElementById("dec").innerHTML="Your income for the month of "+month[11]+" will be Kshs."+zero+".";
+        document.getElementById("Monthlyincome").innerHTML = "Income per month"
+        document.getElementById("jan").innerHTML = "Your income for the month of " + month[0] + " will be Kshs." + one + ".";
+        document.getElementById("feb").innerHTML = "Your income for the month of " + month[1] + " will be Kshs." + zero + ".";
+        document.getElementById("mar").innerHTML = "Your income for the month of " + month[2] + " will be Kshs." + one + ".";
+        document.getElementById("apr").innerHTML = "Your income for the month of " + month[3] + " will be Kshs." + zero + ".";
+        document.getElementById("may").innerHTML = "Your income for the month of " + month[4] + " will be Kshs." + one + ".";
+        document.getElementById("june").innerHTML = "Your income for the month of " + month[5] + " will be Kshs." + zero + ".";
+        document.getElementById("july").innerHTML = "Your income for the month of " + month[6] + " will be Kshs." + one + ".";
+        document.getElementById("aug").innerHTML = "Your income for the month of " + month[7] + " will be Kshs." + zero + ".";
+        document.getElementById("sep").innerHTML = "Your income for the month of " + month[8] + " will be Kshs." + one + ".";
+        document.getElementById("oct").innerHTML = "Your income for the month of " + month[9] + " will be Kshs." + zero + ".";
+        document.getElementById("nov").innerHTML = "Your income for the month of " + month[10] + " will be Kshs." + one + ".";
+        document.getElementById("dec").innerHTML = "Your income for the month of " + month[11] + " will be Kshs." + zero + ".";
     }
 
 }
 
 function reset() {
     location.reload();
-  }
+}
